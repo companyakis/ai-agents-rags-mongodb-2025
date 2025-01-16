72d0540d818ac8f4f27dcc976485788df28a559b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-16 13:07:47 +0300

    Delete part-c-0001-create-milvus-vector-db.py

--- part-c-0001-create-milvus-vector-db.py
+++
@@ -1,7 +0,0 @@
-from pymilvus import MilvusClient
-
-# let's create a local Milvus vector database
-
-local_db = MilvusClient("milvus_learning.db")
-
-print(local_db) # <pymilvus.milvus_client.milvus_client.MilvusClient object at 0x7aa921e0d0c0>
