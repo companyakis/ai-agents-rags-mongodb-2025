3fd94961f73fd73d975435a277c95325e35e8742
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-06 22:43:18 +0300

    Create part-c-0001-type-hints2.py

---
+++ part-c-0001-type-hints2.py
@@ -0,0 +1,5 @@
+weekly_sales: list[int] = [5007, 12440, 8770, 9245]
+
+#weekly_sales.append("Hi") #  "Literal['Hi']" is not assignable to "int"
+
+employees: list[str] = ["Mustafa", "Aygün", "Bilge", "Bengü"]
