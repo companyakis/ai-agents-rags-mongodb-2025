89f46b40feec1f24f2643719c2addcb2f3d7b085
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-12 23:20:46 +0300

    Delete part-c-0001-type-hints3.py

--- part-c-0001-type-hints3.py
+++
@@ -1,7 +0,0 @@
-def create_employee_data(name: str, id: str, department: str, title: str, salary: int) :
-
-    return (name, id, department, title, salary)
-
-emp1 = create_employee_data("Ayhan", "101", "HR", "Senior Expert", 75500)
-
-print(emp1) # ('Ayhan', '101', 'HR', 'Senior Expert', 75500)
