6067a0633de2c7055b8f7f0bd7bed5fa1db4d4cf
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-18 21:05:52 +0300

    Delete part-e-0001-create-embeddings2.py

--- part-e-0001-create-embeddings2.py
+++
@@ -1,7 +0,0 @@
-# Extract the total_tokens from response_dict
-
-print(response_dict["usage"]["total_tokens"])
-
-# Extract the embeddings from response_dict
-
-print(response_dict["data"][0]["embedding"])
