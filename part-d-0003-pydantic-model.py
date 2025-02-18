e2eeb8d0a2f99383a8675394d16d9500af288d59
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-18 22:21:06 +0300

    Delete part-d-0003-pydantic-model.py

--- part-d-0003-pydantic-model.py
+++
@@ -1,8 +0,0 @@
-from datetime import date
-
-from pydantic import BaseModel
-
-class Item(BaseModel):
-    name: str
-    quantity: int = 0 # default value 0
-    expiration: date = None # default None
